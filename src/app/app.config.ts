42034d517fc9f14b6a95b597c350bf51860fe86f
hush <mikhail@kenmoredesign.com>
2025-08-05 07:12:11 +0000

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+
 import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { routes } from './app.routes';
@@ -19,6 +22,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
 
     importProvidersFrom(BrowserAnimationsModule, ModalModule.forRoot()),
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+
+  firebase: {
+    apiKey: 'AIzaSyCRhiHOHxrwXwgud5C3ITPvL2imPQ_qop0',
+    authDomain: 'monster-slayer-143.firebaseapp.com',
+    projectId: 'monster-slayer-143',
+    storageBucket: 'monster-slayer-143.firebasestorage.app',
+    messagingSenderId: '569791427101',
+    appId: '1:569791427101:web:6890eb64cfe51f3b928cf4',
+    measurementId: 'G-HNV3F1K3PF',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
